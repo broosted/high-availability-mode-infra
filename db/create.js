7e5702df6ebd2963f16b510439d66d060121b179
broosted <matrix.10.tad@gmail.com>
2022-05-23 20:51:59 -0700

    made db work in cluster

--- db/create.js
+++ db/create.js
@@ -1,4 +1,15 @@
-use data
+db = new Mongo().getDB("admin");
+db.createUser({
+    user: "admin",
+    pwd: "password",
+    roles: [
+      {
+        role: "clusterAdmin",
+        db: "admin"
+      }
+    ]
+  });
+db = new Mongo().getDB("data");
 db.createCollection("userData")
 db.userData.insert({"name":"John", "phone": 2390932493})
 db.userData.insert({"name":"kenny","phone":3892637832})
